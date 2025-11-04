635209009b84b2da5c9ff9a95e4acb1a518a0ac4
UserloveCoding04 <nhthuc2004@gmail.com>
2025-11-04 20:40:16 +0700

    update auto create tables in database

--- app.py
+++ app.py
@@ -224,4 +224,6 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host='localhost', port=5000, debug=True)
